9be4290c41f5b60d8f432cb9359fca5fafb6cd60
putrasempalas1222-eng <putrasempalas1222@gmail.com>
2026-04-14 01:25:03 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,23 +1,19 @@
 export default async function handler(req, res) {
   try {
-    const body = req.body;
-
     const response = await fetch("https://us-central1-conquer-apps-2ad61.cloudfunctions.net/prod/api.live", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(body)
+      body: JSON.stringify(req.body)
     });
 
     const data = await response.json();
 
     res.status(200).json(data);
-
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({
-      error: "Server error",
-      detail: error.message
+      error: err.message
     });
   }
 }
